86c327c1645d3369a907408ced7c067eab063d22
gaoqinghuang <gaoqinghuang@xiaoyouzi.com>
2018-11-20 10:47:23 +0800

    struct

---
+++ structInlaid.go
@@ -0,0 +1,58 @@
+package main
+import (
+	"fmt"
+)
+
+type BasicColor struct {
+	R,G,B float32
+}
+
+type Color struct{
+	BasicColor
+	Alpha float32
+	R float32
+}
+
+// func main() {
+// 	var c Color
+// 	c.BasicColor.R=1
+// 	c.R = 2
+// 	c.G = 1
+// 	c.B = 0
+
+// 	c.Alpha = 1
+
+// 	fmt.Printf("%+v",c)
+// }
+
+//匿名结构体的定义
+
+type Wheel struct{
+	Size int
+}
+
+type Car struct{
+	Wheel
+	Engine struct{
+		Power int
+		Type string
+	}
+}
+
+func main() {
+	car:= Car{
+		Wheel: Wheel{
+			Size: 10,
+		},
+		Engine: struct{
+			Power int
+			Type string
+		}{
+			Power: 143,
+			Type: "1.4T",
+		},
+	}
+	fmt.Println(car)
+	// fmt.Printf("%+v",car)
+}
+
---
+++ structJicheng.go
@@ -0,0 +1,36 @@
+package main
+import (
+	"fmt"
+)
+
+type Flying struct {
+}
+
+func (f *Flying) Fly(){
+	fmt.Println("I can fly")
+}
+
+type Walkable struct{
+}
+
+func (w *Walkable) Walk(){
+	fmt.Println("I can walk")
+}
+
+type Human struct{
+	Walkable
+}
+type Bird struct{
+	Walkable
+	Flying
+}
+
+func main(){
+	b := new(Bird)
+	fmt.Println("Bird:")
+	b.Fly()
+	b.Walk()
+	c := new(Human)
+	fmt.Println("Human:")
+	c.Walk()
+}
