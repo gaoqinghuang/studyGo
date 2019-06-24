7f0c007406a71dd9547088ad1d558a154b163b62
gaoqinghuang <gaoqinghuang@xiaoyouzi.com>
2019-06-24 14:24:47 +0800

    strconv 包

--- src-dome/strconv/strconv.go
+++ src-dome/strconv/strconv.go
@@ -1,1 +1,19 @@
-package strconv
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	//总体没看到什么有用的，就下面3个经常用到
+
+	//数字转字符串
+	fmt.Println(strconv.Itoa(10))
+	//字符串转数字
+	fmt.Println(strconv.Atoi("12"))
+
+	//int或者uint的位数
+	//fmt.Println(strconv.IntSize)
+}
+
