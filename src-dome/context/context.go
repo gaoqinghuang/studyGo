3b4df00561b8461fcbee4c40ed8dc1e022fab02d
gaoqinghuang <gaoqinghuang@xiaoyouzi.com>
2019-06-26 14:44:22 +0800

    context 包

---
+++ src-dome/context/context.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"context"
+	"fmt"
+)
+
+func main() {
+	//像是一个总控制的包
+
+
+
+	type favContextKey string
+
+	f := func(ctx context.Context, k favContextKey) {
+		if v := ctx.Value(k); v != nil {
+			fmt.Println("found value:", v)
+			return
+		}
+		fmt.Println("key not found:", k)
+	}
+
+	k := favContextKey("language")
+	ctx := context.WithValue(context.Background(), k, "Go")
+
+	f(ctx, k)
+	f(ctx, favContextKey("color"))
+}
