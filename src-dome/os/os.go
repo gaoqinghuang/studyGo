fb02bb06ffdc866fef3bafa85be342fcf8a8de40
gaoqinghuang <gaoqinghuang@xiaoyouzi.com>
2019-06-27 09:19:17 +0800

    os包

---
+++ src-dome/os/os.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	shellPath := "/home/xx/test.sh"
+	argv := make([]string, 1)
+	attr := new(os.ProcAttr)
+	newProcess, err := os.StartProcess(shellPath, argv, attr) //运行脚本
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println("Process PID", newProcess.Pid)
+	processState, err := newProcess.Wait() //等待命令执行完
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println("processState PID:", processState.Pid()) //获取PID
+	fmt.Println("ProcessExit:", processState.Exited())   //获取进程是否退出
+
+
+	//file, _ := os.OpenFile("xiaogao.txt", os.O_APPEND, 0)
+	//defer file.Close()
+	//
+	//data := make([]byte, 1024)
+	//n, _ := file.Read(data)
+	//fmt.Println(file.WriteString("\n"))
+	//fmt.Println(string(data[:n]))
+
+}
